1612134262305e1469227291d4c54fe61b9a3f63
alXna93 <43002121+alXna93@users.noreply.github.com>
2019-02-18 14:11:43 +0000

    added player movement & speed
    added moveSpeed float to script and controls for moving left, right, up and down.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public Rigidbody2D theRB;
+    public float moveSpeed;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+      
 	// Update is called once per frame
 	void Update () {
-		
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
 	}
 }
