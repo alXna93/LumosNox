a2942b998424a48f046c54eb85b035326fe4cf3b
alXna <38910531+alXna@users.noreply.github.com>
2019-03-08 15:07:49 +0000

    updated camera controller to stop at edges of map

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Tilemaps;
 public class CameraController : MonoBehaviour {
 
     public Transform target;
+
+    public Tilemap theMap;
+
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
+
+    private float halfHeight;
+    private float halfWidth;
 
 
 	// Use this for initialization
@@ -12,11 +20,21 @@ public class CameraController : MonoBehaviour {
 
         target = PlayerController.instance.transform;
 
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
+
+        bottomLeftLimit = theMap.localBounds.min + new Vector3(halfWidth, halfHeight, 0f);
+        topRightLimit = theMap.localBounds.max + new Vector3(-halfWidth, -halfHeight, 0f);
+
+
 	}
 	
 	// LateUpdate is called once per frame after update
 	void LateUpdate () {
 
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        //keep camera inside bounds
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
 	}
 }
