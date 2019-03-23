4fad90297608675a64b58468d1c30febef4396a7
alXna93 <43002121+alXna93@users.noreply.github.com>
2019-03-23 13:56:31 +0000

    Added fade in/out
    Added fade in and fade out screen for when the player transitions into our out of each level

--- Assets/Scripts/AreaExit.cs
+++ Assets/Scripts/AreaExit.cs
@@ -12,6 +12,9 @@ public class AreaExit : MonoBehaviour {
 
     public AreaEntrance theEntrance;
 
+    public float waitToLoad = 1f;
+    private bool shouldLoadAfterFade;
+
 	// Use this for initialization
 	void Start () {
         theEntrance.transitionName = areaTransitionName;
@@ -19,14 +22,26 @@ public class AreaExit : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (shouldLoadAfterFade)
+        {
+            waitToLoad -= Time.deltaTime;
+
+            if(waitToLoad <= 0)
+            {
+                shouldLoadAfterFade = false;
+                SceneManager.LoadScene(areaToLoad);
+            }
+        }
+      
 	}
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-            SceneManager.LoadScene(areaToLoad);
+            //SceneManager.LoadScene(areaToLoad);
+            shouldLoadAfterFade = true;
+            UIFade.instance.FadeToBlack();
 
             PlayerController.instance.areaTransitionName = areaTransitionName;
         }
