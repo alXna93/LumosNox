1bb4fcd238f0eee94e62a0dd3a80b125aee5a585
alXna93 <43002121+alXna93@users.noreply.github.com>
2019-03-26 12:36:49 +0000

    Dialog
    added dialog manager&activator script, when player walks to NPC and clicks, the dialog box appears, shows different lines for each click until array finished

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -15,16 +15,56 @@ public class DialogueManager : MonoBehaviour {
 
     public int currentLine;
 
+    private bool justStarted;
+
+    public static DialogueManager instance;
+
 	// Use this for initialization
 	void Start () {
 
-        dialogText.text = dialogLines[currentLine];
+        instance = this;
+        //dialogText.text = dialogLines[currentLine];
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+        if (dialogBox.activeInHierarchy)
+        {
+            if (Input.GetButtonUp("Fire1"))
+            {
+                if (!justStarted)
+                {
+                    currentLine++;
 
+                    if (currentLine >= dialogLines.Length)
+                    {
+                        dialogBox.SetActive(false);
+                    }
+                    else
+                    {
+                        dialogText.text = dialogLines[currentLine];
+                    }
+
+                }
+                else
+                {
+                    justStarted = false;
+                }
+            }
+        }
 
 	}
+
+    public void ShowDialog(string[] newLines)
+    {
+        dialogLines = newLines;
+
+        currentLine = 0;
+
+        dialogText.text = dialogLines[0];
+        dialogBox.SetActive(true);
+        justStarted = true;
+
+    }
 }
