44c8aed8976ceefb8018130e4fdebff6e3637a3c
alXna93 <43002121+alXna93@users.noreply.github.com>
2019-05-07 18:20:26 +0100

    added menu script
    Added code to open and close the menu in the game

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenu : MonoBehaviour {
+
+
+    public GameObject theMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire2"))
+        {
+            if(theMenu.activeInHierarchy)
+            {
+                theMenu.SetActive(false);
+            } else
+            {
+                theMenu.SetActive(true);
+            }
+        }
+	}
+}
