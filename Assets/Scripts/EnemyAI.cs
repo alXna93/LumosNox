bbb8e34f89add900f40df1d536b389875cc962a0
alXna93 <43002121+alXna93@users.noreply.github.com>
2019-05-22 00:04:21 +0100

    started adding enemy 1
    created animations for enemy 1 (depression) also created a script for enemy AI and a seperate script for the specific enemy

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+
+    public int health;
+    public string enemyName;
+    public int baseAttack;
+    public float EnemySpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
