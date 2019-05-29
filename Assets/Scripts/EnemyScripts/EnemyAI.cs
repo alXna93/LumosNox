6d40093b24f66920883064195f5a5c217e2a6e7e
alXna93 <43002121+alXna93@users.noreply.github.com>
2019-05-29 06:10:39 +0100

    made enemy death effect

--- Assets/Scripts/EnemyScripts/EnemyAI.cs
+++ Assets/Scripts/EnemyScripts/EnemyAI.cs
@@ -18,6 +18,7 @@ public class EnemyAI : MonoBehaviour
     public string enemyName;   
     public float EnemySpeed;
     public int baseAttack;
+    public GameObject deathEffect;
 
     private void Awake()
     {
@@ -29,7 +30,18 @@ public class EnemyAI : MonoBehaviour
         health -= damage;
         if (health <= 0)
         {
+            DeathEffect();
             this.gameObject.SetActive(false);
+        }
+    }
+
+    private void DeathEffect()
+    {
+        if (deathEffect != null)
+        {
+            GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 1f);
+                
         }
     }
     public void Knock(Rigidbody2D myRigidbody, float knockTime, float damage)
