1e57e4ca4bed91c80c2a234255f61997099fe14b
alXna93 <43002121+alXna93@users.noreply.github.com>
2019-05-29 03:47:19 +0100

    created clue
    added in a clue icon for when the player is near an interactable game object e.g a NPC or a sign

---
+++ Assets/Scripts/PlayerScripts/Clue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clue : MonoBehaviour {
+
+    public GameObject clue;
+    public bool clueActive = false;
+
+    public void ChangeClue()
+    {
+        clueActive = !clueActive;
+        if(clueActive)
+        {
+            clue.SetActive(true);
+        }
+        else
+        {
+            clue.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/SceneScripts/Interactables.cs
+++ Assets/Scripts/SceneScripts/Interactables.cs
@@ -2,34 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogActivator : Interactables {
+public class Interactables : MonoBehaviour {
 
-    public string[] lines;
-
+    public bool isPerson = true;
+    public Signal clue;
     private bool canActivate;
 
-   
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(canActivate && Input.GetButtonDown("Fire1") && !DialogueManager.instance.dialogBox.activeInHierarchy)
-        {
-            DialogueManager.instance.ShowDialog(lines, isPerson);
-        }
+		
 	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && !other.isTrigger)
+        {
+            clue.Raise();
+            canActivate = true;
+        }
+    }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == "Player" && !other.isTrigger)
         {
             clue.Raise();
-            canActivate = false;
+           
         }
     }
-
-
 }
